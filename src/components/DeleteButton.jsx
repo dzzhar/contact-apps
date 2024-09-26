e1728963eb5b74ff0499ecbb39081dc020a9448a
dzzhar <zharifahdzikra@gmail.com>
2024-09-26 20:27:05 +0700

    case study contacts app

---
+++ src/components/DeleteButton.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable react/prop-types */
+function DeleteButton({ id, onDelete }) {
+  return (
+    <button className="contact-item__delete" onClick={() => onDelete(id)}>
+      X
+    </button>
+  );
+}
+
+export default DeleteButton;
